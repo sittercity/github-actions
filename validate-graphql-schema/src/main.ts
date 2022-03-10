815fd570650f3d1712634725871d3bbceb0ecb7a
Usman Asif <93947029+dev-ltd@users.noreply.github.com>
2022-03-11 01:40:06 +0500

    [ch80756]: Implemented custom github action for graphql schema validation (#12)
    * [ch80756]: Initial commit
    
    * [ch80756]: Created initial build
    
    * [ch80756]: Implemented functionalty to comment on PR
    
    * [ch80756]: Implemented eslint configs and fixed linting erros
    
    * [ch80756]: fix typescript errors
    
    * [ch80756]: run build
    
    * [ch80756]: version update
    
    * [ch80756]: fixed node dependency issues
    
    * WIP
    
    * use vercel/ncc
    
    * revert commented code
    
    * [ch80756]: Added github actions input and output functionality
    
    * [ch80756]: Updated readme
    
    * Update node version
    
    * Added working directory
    
    * minor changes
    
    * revert console.log
    
    * version updated
    
    * [ch80756]: Updated warning message and readme
    
    * [ch80756]: Updated warning message
    
    * [ch80756]: Updated readme
    
    * [ch80756]: PR reviews
    
    Co-authored-by: Justus Renton <jrenton@sittercity.com>

---
+++ validate-graphql-schema/src/main.ts
@@ -0,0 +1,56 @@
+import * as core from '@actions/core'
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { Change, diff } from '@graphql-inspector/core'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { UrlLoader } from '@graphql-tools/url-loader'
+
+import { comment } from './pr'
+
+/**
+ * Executes the main action. It includes the main business logic and is the
+ * primary entry point. It is documented inline.
+ */
+async function run(): Promise<void> {
+
+  try {
+    const workingDirectory = core.getInput('working-directory') || '.';
+    const schema_path = core.getInput('schema', {required: true})
+    const schema_full_path = `${workingDirectory}/${schema_path}`
+    const endpoint_path = core.getInput('endpoint', {required: true})
+
+    const schema = loadSchemaSync(schema_full_path, {
+      loaders: [new GraphQLFileLoader()],
+    })
+
+    // load from endpoint
+    const endpoint = loadSchemaSync(endpoint_path, {
+      loaders: [new UrlLoader()],
+    })
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const changes: Change[] = await diff(endpoint, schema)
+    const messages = changes.filter(c => c.criticality.level !== 'NON_BREAKING').map(c => `${c.type}: ${c.path}`)
+
+    if (messages.length != 0) {
+      let warningMessage = '#### ⚠️ Warning: A breaking change has been made to the graphql schema. Please confirm that no graphql clients still rely on the item being changed.\nHere, is the summary:\n\n - '
+      warningMessage += messages.join('\n - ')
+
+      core.setOutput('warning_message', warningMessage);
+
+      await comment(warningMessage, core.getInput('github-token'));
+
+      core.warning(warningMessage);
+
+      return;
+    }
+
+    core.info(
+      '✅ Great Work! No breaking changes detected in graphql schema.',
+    );
+  } catch (error) {
+    console.log((error as Error).message)
+  }
+}
+
+run()
